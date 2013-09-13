3854a235ab0cbe3a43a7af710d83809e54b33a3c
Troy Swanson <troyswanson@me.com>
2013-09-12 23:34:09 -0500

    Save some changes to Casper file

--- _tests/test.js
+++ _tests/test.js
@@ -1,7 +1,9 @@
 var casper = require('casper').create();
 
-casper.start('http://localhost:9292/', function() {
-    this.echo(this.getTitle());
+casper.start('http://localhost:9292/', function () {
+  this.echo(this.evaluate(function () {
+    return document.links.length // returns a NodeList containing HTMLAnchorElement(s)
+  }));
 });
 
 casper.run();
