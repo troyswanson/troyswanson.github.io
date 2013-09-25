56d30a3781fd001d5d625c71bcc33067ecc5f132
Troy Swanson <troyswanson@me.com>
2013-09-24 23:56:21 -0500

    Use port 4000

--- _tests/test.js
+++ _tests/test.js
@@ -1,6 +1,6 @@
 var casper = require('casper').create();
 
-casper.start('http://localhost:9292/', function () {
+casper.start('http://localhost:4000/', function () {
   this.echo(this.evaluate(function () {
     return document.links.length // returns a NodeList containing HTMLAnchorElement(s)
   }));
